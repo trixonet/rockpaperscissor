833459ebc9e39a95e33941b3f59b9e3494e1fc94
MooT29 <49246693+MooT29@users.noreply.github.com>
2019-04-03 12:40:17 -0400

    Update main.js
    Added code for TIE and YOU WIN as wella few if statements

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-
+function start (){
+    println ("1. Earth");
+    println ("2. Fire");
+    println ("3. Water");
+    
+    var intro = readInt ("Welcome to Elements! Test your luck by choosing an element, which one shall you chose? ");
+    
+    
+    var roll = Randomizer.nextInt(1,3);
+    
+    println ("Computer picks: " + roll );  
+    if (intro == roll){
+        println ("TIE");
+    }
+    if (intro == 3 && roll == 2){
+        println ("YOU WIN");
+    }
+}
