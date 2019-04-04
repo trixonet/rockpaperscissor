db363972cf74af82c47e9277d30eaf306cf42469
Brandon Caballeros <branik0914@hotmail.com>
2019-04-04 12:37:59 -0400

    Created new sequences, added comments

--- main.js
+++ main.js
@@ -8,11 +8,38 @@ function start (){
     
     var roll = Randomizer.nextInt(1,3);
     
-    println ("Computer picks: " + roll );  
+    println ("Computer picks: " + roll ); 
+    
+    //Tie both numbers (player and computer)
     if (intro == roll){
         println ("TIE");
     }
-    if (intro == 3 && roll == 2){
+    
+    //Earth to water
+    if (intro == 1 && roll == 3){
         println ("YOU WIN");
+    }
+    
+    //Fire to earth
+    if (intro == 2 && roll == 1){
+        println ("YOU WIN");
+    }
+    
+    //Water to fire
+    if (intro == 3 && roll == 2){
+       println  ("YOU WIN");
+    }
+    
+     // Bottom Section are Losing Seqeunces for the player sequences   
+    if (intro == 1 && roll == 2){
+       println  ("YOU LOSE"); 
+    }
+    
+    if (intro == 2 && roll == 3){
+       println ("YOU LOSE");
+    }
+    
+    if (intro == 3 && roll == 1){
+       println ("YOU LOSE"); 
     }
 }
