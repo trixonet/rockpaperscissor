ea3e7d1280c19c1cc902619692e6e08ee29f290e
Brandon Caballeros <branik0914@hotmail.com>
2019-04-23 12:37:07 -0400

    Added new functions for pictures

--- main.js
+++ main.js
@@ -150,7 +150,95 @@ function earthToWaterPic(){
      add(water);
 }
 
-function 
+function fireToEarthPic(){
+    var dirt = new WebImage("https://codehs.com/uploads/5c5b03e5830f4a1db397f2503291cfdd");
+    dirt.setSize(138, 175);
+    dirt.setPosition(20, 35);
+
+    var fire = new WebImage("https://codehs.com/uploads/01b7bc8d1f78f61f0e107d69c68f4760");
+    fire.setSize(138, 175);
+    fire.setPosition(0, 35);
+
+    var water = new WebImage("https://codehs.com/uploads/229bfa3a8677c3aa6e7d165abd870547");
+    water.setSize(110, 155);
+    water.setPosition(250, 35);
+     
+     add(dirt); 
+     remove(fire);
+     add(water);
+}
+
+function waterToFirePic(){
+    var dirt = new WebImage("https://codehs.com/uploads/5c5b03e5830f4a1db397f2503291cfdd");
+    dirt.setSize(138, 175);
+    dirt.setPosition(20, 35);
+
+    var fire = new WebImage("https://codehs.com/uploads/01b7bc8d1f78f61f0e107d69c68f4760");
+    fire.setSize(138, 175);
+    fire.setPosition(0, 35);
+
+    var water = new WebImage("https://codehs.com/uploads/229bfa3a8677c3aa6e7d165abd870547");
+    water.setSize(110, 155);
+    water.setPosition(250, 35);
+     
+     add(dirt); 
+     remove(fire);
+     add(water);
+}
+
+function loseEarthToFirePic(){
+    var dirt = new WebImage("https://codehs.com/uploads/5c5b03e5830f4a1db397f2503291cfdd");
+    dirt.setSize(138, 175);
+    dirt.setPosition(20, 35);
+
+    var fire = new WebImage("https://codehs.com/uploads/01b7bc8d1f78f61f0e107d69c68f4760");
+    fire.setSize(138, 175);
+    fire.setPosition(0, 35);
+
+    var water = new WebImage("https://codehs.com/uploads/229bfa3a8677c3aa6e7d165abd870547");
+    water.setSize(110, 155);
+    water.setPosition(250, 35);
+     
+     add(dirt); 
+     remove(fire);
+     add(water);
+}
+
+function loseFireToWaterPic(){
+    var dirt = new WebImage("https://codehs.com/uploads/5c5b03e5830f4a1db397f2503291cfdd");
+    dirt.setSize(138, 175);
+    dirt.setPosition(20, 35);
+
+    var fire = new WebImage("https://codehs.com/uploads/01b7bc8d1f78f61f0e107d69c68f4760");
+    fire.setSize(138, 175);
+    fire.setPosition(0, 35);
+
+    var water = new WebImage("https://codehs.com/uploads/229bfa3a8677c3aa6e7d165abd870547");
+    water.setSize(110, 155);
+    water.setPosition(250, 35);
+     
+     add(dirt); 
+     remove(fire);
+     add(water);
+}
+
+function loseWaterToEarth(){
+    var dirt = new WebImage("https://codehs.com/uploads/5c5b03e5830f4a1db397f2503291cfdd");
+    dirt.setSize(138, 175);
+    dirt.setPosition(20, 35);
+
+    var fire = new WebImage("https://codehs.com/uploads/01b7bc8d1f78f61f0e107d69c68f4760");
+    fire.setSize(138, 175);
+    fire.setPosition(0, 35);
+
+    var water = new WebImage("https://codehs.com/uploads/229bfa3a8677c3aa6e7d165abd870547");
+    water.setSize(110, 155);
+    water.setPosition(250, 35);
+     
+     add(dirt); 
+     remove(fire);
+     add(water);
+}
 
 
 
